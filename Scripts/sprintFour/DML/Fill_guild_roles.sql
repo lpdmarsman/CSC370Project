f12e2c16fc68f82eb9d353a093ac52a25408fc5f
alexanderw885 <alexanderw885@gmail.com>
2024-07-08 18:02:03 -0700

    added script to fill guild_roles table with existing data

---
+++ Scripts/sprintFour/DML/Fill_guild_roles.sql
@@ -0,0 +1,51 @@
+START TRANSACTION;
+-- Add existing players and roles into guild_roles
+INSERT INTO `guild_roles`(`player_id`,`guild_role`)
+(
+  SELECT `player_id`, `guild_role`
+  FROM `players`
+);
+
+-- Give all permissions to Guild Masters
+UPDATE `guild_roles`
+SET `armory_access` = true,
+`invite_permission` = true,
+`ban_permission` = true,
+`promote_permission` = true
+WHERE `guild_role` LIKE 'Guild Master';
+
+-- Give armory_access and invite permission to Crafters
+UPDATE `guild_roles`
+SET `armory_access` = true,
+`invite_permission` = true
+WHERE `guild_role` LIKE 'Crafter';
+
+-- Give invite permission to Members
+UPDATE `guild_roles`
+SET `invite_permission` = true
+WHERE `guild_role` LIKE 'Member';
+
+-- Give armory access, and invite and ban permission to Officers
+UPDATE `guild_roles`
+SET `armory_access` = true,
+`invite_permission` = true,
+`ban_permission` = true
+WHERE `guild_role` LIKE 'Officer';
+
+-- Give armory access and invite permission to Raiders
+UPDATE `guild_roles`
+SET `armory_access` = true,
+`invite_permission` = true
+WHERE `guild_role` LIKE 'Raider';
+
+-- Recruits get no permission
+
+-- Give armory access and invite permissions to Veterans
+UPDATE `guild_roles`
+SET `armory_access` = true,
+`invite_permission` = true
+WHERE `guild_role` LIKE 'Veteran';
+
+SELECT * FROM `guild_roles`
+ORDER BY `guild_role`;
+COMMIT;
